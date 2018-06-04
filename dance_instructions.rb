9470f291408a503ea0355cd2e5b37d3b3252dad5
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-04 23:24:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,10 +4,13 @@ def staring_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end 
 
-def
-  puts "lift right foot"
-  puts "return right foot to the ground"
-  puts "finishing with a small skip-step backward"
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+
+def skip_step
   puts "lower left foot to the ground"
   puts "bounce left foot back up slightly, kicking it a few inches back"
   
