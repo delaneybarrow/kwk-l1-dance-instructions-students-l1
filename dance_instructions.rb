bb797bd52161be03ef071ae4877493ed0fd1928c
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-04 23:34:21 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -10,6 +10,12 @@ def base_footwork
   puts "Finishing with a small skip-step backward"
 end
 
+def skip_step_right
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+end
+
 def skip_step_left
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
@@ -27,11 +33,7 @@ def lasso
   puts "Lift right arm up and point right elbow diagonally to the right"
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end
-def skip_step_right
-  puts "Lower right foot to the ground"
-  puts "Bounce right foot back up slightly, kicking it a few inches back"
-  puts "Right, Left, Right and Right"
-end
+
 
 
   
