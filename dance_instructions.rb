b51068dbbe1efbee3a4309d8145fb651b061e153
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-04 23:31:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -24,6 +24,8 @@ end
 
 def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, ointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 def skip_step_right
   puts "Lower right foot to the ground"
   puts "Bounce right foot back up slightly, kicking it a few inches back"
