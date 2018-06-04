4ee666f3eb5061a307b90000c2b436e96ee8e76b
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-04 23:20:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,4 +3,8 @@
 def dance_instructions
   puts "plant legs far apart, bend knees slightly and keep posture loose"
   puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+  puts "lower left foot to the ground"
+  puts "bounce left foot back up slightly, kicking it a few inches back"
   
